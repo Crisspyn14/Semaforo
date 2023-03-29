1cf03b11745375a0f7476d6300a42ddcab71b88c
Crisspyn14 <126734893+Crisspyn14@users.noreply.github.com>
2023-03-29 19:49:52 -0400

    Add files via upload

---
+++ Semaforo Tarea Lizandro.c
@@ -0,0 +1,42 @@
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <unistd.h>
+ #include <semaphore.h>
+ #include <fcntl.h>
+
+int main() {
+  sem_t *sem;
+  pid_t pid;
+
+  sem = sem_open("/mysem", O_CREAT, 0644, 1);
+  if (sem == SEM_FAILED) {
+    perror("Error al crear el semáforo");
+    exit(EXIT_FAILURE);
+  }
+
+  pid = fork();
+  if (pid < 0) {
+    perror("Error al crear el proceso hijo");
+    exit(EXIT_FAILURE);
+  } else if (pid == 0) {
+
+
+    printf("EL Proceso hijo esperando al semáforo...\n");
+    sem_wait(sem);
+    printf("El Proceso hijo dentro de la sección crítica\n");
+    sleep(5);
+    sem_post(sem);
+    printf("EL Proceso hijo ha liberado el semáforo\n");
+    exit(EXIT_SUCCESS);
+
+  } else {
+
+    printf("EL Proceso padre esperando al semáforo...\n");
+    sem_wait(sem);
+    printf("EL Proceso padre dentro de la sección crítica\n");
+    sleep(5);
+    sem_post(sem);
+    printf("EL Proceso padre ha liberado el semáforo\n");
+    exit(EXIT_SUCCESS);
+  }
+}
